7f59644a01d7a9d8ce39051a5f8b026f8e9ed5db
Ellis <ellislunnon@gmail.com>
2021-02-09 14:35:11 +0000

    Saving changes before updating immer

---
+++ pagemail/src/sitemap/sitemap-routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route } from 'react-router';
+ 
+export default (
+    <Route>
+        <Route path='/' />
+        <Route path="/login" />
+        <Route path="/signup" />
+        <Route path="/pages" />
+        <Route path="/user" />
+        <Route path="/signout" />
+    </Route>
+);
